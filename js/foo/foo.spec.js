9ca247d7c5a9904d5958e4d374b0b77b07c41628
Jem Young <jemuel.young@gmail.com>
2016-04-12 21:04:06 -0700

    Update

---
+++ js/foo/foo.spec.js
@@ -0,0 +1,21 @@
+describe('calcNums', function() {
+  describe('This is a test', function() {
+    it('should do something', function() {
+      expect(typeof calcNums(358)).toBe('number');
+    });
+  });
+});
+describe('foo2', function() {
+  describe('This is a test', function() {
+    it('should do something', function() {
+      expect(typeof foo2('bqRCBvLwWw')).toBe('string');
+    });
+  });
+});
+describe('foo3', function() {
+  describe('This is a test', function() {
+    it('should do something', function() {
+      expect(Array.isArray(foo3([6,3,9]))).toBe(true);
+    });
+  });
+});
---
+++ js/foo/foo2.spec.js
@@ -0,0 +1,7 @@
+describe('multiply', function() {
+  describe('This is a test', function() {
+    it('should do something', function() {
+      expect(typeof multiply(56, 238)).toBe('number');
+    });
+  });
+});
